3cc3d74476c2fbf4ae415297c80bd56b3090e71d
Jericho Tolentino <jtolentino12@student.rccd.edu>
2025-03-12 18:03:21 -0700

    Outlined the book, library, user, faculty, and student classes

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+// Book Class
+class Book {
+
+};
+
+// Library Class
+class Library {
+
+};
+
+// User Class
+class User {
+
+};
+
+// Student Class
+class Student : public User {
+
+};
+
+// Faculty Class
+class Faculty : public User {
+
+};
+
+int main() {
+
+
+
+
+
+
+
+    return 0;
+}
+
